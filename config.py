cc932183a21bdf13ee69f0c4865726084132725e
Endriw <endriwxavier@gmail.com>
2025-01-05 14:58:16 -0300

    fix: remove duplicate files

--- config.py
+++
@@ -1,9 +0,0 @@
-# Store database configurations.
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
