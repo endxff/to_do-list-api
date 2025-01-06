93b63abe194755a7e0f4dbbdb6bb0a50368f5fc9
Endriw <endriwxavier@gmail.com>
2025-01-06 11:25:39 -0300

    feat: inital app page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='container'>
+			<div className='mx-auto w-[80vw] mt-5'>
+				<div className='flex gap-10 items-center h-[30px]'>
+					{/* Create task */}
+					<div className='bg-lime-400 rounded-xl h-full flex items-center'>
+						<a
+							className='p-4'
+							href='/new'
+						>
+							New task
+						</a>
+					</div>
+
+					{/* Search task */}
+					<div className='h-full'>
+						<input
+							className='w-[30vw] bg-slate-100 rounded-xl h-full px-4 mr-4'
+							type='search'
+							name='Task'
+							id=''
+							placeholder='Search task'
+						/>
+						<button>Search</button>
+					</div>
+				</div>
+				{/* Task list */}
+				<div className='w-[80vw] mt-10 bg-slate-100 rounded-xl p-2'>
+					<table className='w-full'>
+						<thead className='text-left'>
+							<tr>
+								<th>ID</th>
+								<th>Task</th>
+								<th>Description</th>
+								<th>Done</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr className='border-t-2'>
+								<td>8412398</td>
+								<td>Cleaning</td>
+								<td>Clean everything</td>
+								<td>
+									<input
+										type='checkbox'
+										name='done'
+										id=''
+									/>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
